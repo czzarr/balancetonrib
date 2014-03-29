30658e3c9d8f0fbd5a740d48dd076928b66bafeb
Stanislas Marion <stanislas.marion@gmail.com>
2014-03-29 12:13:54 +0100

    friend dropdown works with select2

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var jquery = require('jquery')
 require('select2')
+var request = require('browser-request')
 
-$(document).ready(function () {
-  $('#e1').select2()
+
+request("https://graph.facebook.com/me?access_token=CAALZCAspK6yMBAIXf75wJQSb9m34GM4lo7V2nlaZCh0imrFxbZCZBwDuodskYWnfcoMUocSAeMOZAVQCorZAQV61sZCR4l5KDTt2Hj9vAbKGLChsIFXJ80zKyrzpHBmdlBKRbZC0C0YY6156FUXwkNeLLXtYUj3PTMQWmbUGzH8QIRhD8ZC7LKIhamZAMrZAvJY2bgZD&fields=friends.fields(name,picture)", function (err, res, body) {
+  function format (item) {
+    return item.name
+  }
+  data = JSON.parse(body).friends.data
+  $('#e1').select2({
+    data: { results: data, text: 'name' },
+    formatSelection: format,
+    formatResult: format
+  })
 })
