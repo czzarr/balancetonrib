945c38fd6471f069faf0328eda224f2549ab34ce
Stanislas Marion <stanislas.marion@gmail.com>
2014-03-29 13:58:56 +0100

    working with selectize

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 var jquery = require('jquery')
-require('select2')
+require('selectize')
 var request = require('browser-request')
 
+var token = '?access_token=CAACEdEose0cBALMZCN4zugwiIAZCIiJGMT4l0ZBVYL9OXz79NVm2K7HoEyaGtQAL7OcPrMgDdZAGqhDs74AP2BPYVq4hXiQblct9afUZCmczlmebtdzLZCUoynZBipfLD2S3Fs5kJYf9LfV4TNCvOrWysjGBgFqeHdvjVUqnS2i519zqxX7ESRCl6Ni9xxKFyAZD'
+var endpoint = 'https://graph.facebook.com/me'
+var query = '&fields=friends.fields(name,picture)'
 
-request("https://graph.facebook.com/me?access_token=CAALZCAspK6yMBAIXf75wJQSb9m34GM4lo7V2nlaZCh0imrFxbZCZBwDuodskYWnfcoMUocSAeMOZAVQCorZAQV61sZCR4l5KDTt2Hj9vAbKGLChsIFXJ80zKyrzpHBmdlBKRbZC0C0YY6156FUXwkNeLLXtYUj3PTMQWmbUGzH8QIRhD8ZC7LKIhamZAMrZAvJY2bgZD&fields=friends.fields(name,picture)", function (err, res, body) {
-  function format (item) {
-    return item.name
-  }
+request(endpoint+token+query, function (err, res, body) {
   data = JSON.parse(body).friends.data
-  $('#e1').select2({
-    data: { results: data, text: 'name' },
-    formatSelection: format,
-    formatResult: format
+  $('#e1').selectize({
+    create: false,
+    valueField: 'name',
+    searchField: 'name',
+    labelField: 'name',
+    load: function (query, callback) {
+      callback(data)
+    },
+    render: {
+      option: function (item, escape) {
+            return '<div>' +
+                '<span class="title">' +
+                    '<span class="name">' + escape(item.name) + '</span>' +
+                '</span>' +
+            '</div>';
+      }
+    }
+
   })
 })
