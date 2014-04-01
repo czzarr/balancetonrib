ec1415574f215f54e9f80465d162721aa0e269e1
Stanislas Marion <stanislas.marion@gmail.com>
2014-04-01 02:37:38 +0200

    woohoo it works, need to do some cleanup now

--- client/index.js
+++ client/index.js
@@ -1,30 +1,5 @@
 var jquery = require('jquery')
 require('selectize')
-var request = require('browser-request')
-
-var token = '?access_token=CAACEdEose0cBALMZCN4zugwiIAZCIiJGMT4l0ZBVYL9OXz79NVm2K7HoEyaGtQAL7OcPrMgDdZAGqhDs74AP2BPYVq4hXiQblct9afUZCmczlmebtdzLZCUoynZBipfLD2S3Fs5kJYf9LfV4TNCvOrWysjGBgFqeHdvjVUqnS2i519zqxX7ESRCl6Ni9xxKFyAZD'
-var endpoint = 'https://graph.facebook.com/me'
-var query = '&fields=friends.fields(name,picture)'
-
-request(endpoint+token+query, function (err, res, body) {
-  data = JSON.parse(body).friends.data
-  $('#e1').selectize({
-    create: false,
-    valueField: 'name',
-    searchField: 'name',
-    labelField: 'name',
-    load: function (query, callback) {
-      callback(data)
-    },
-    render: {
-      option: function (item, escape) {
-            return '<div>' +
-                '<span class="title">' +
-                    '<span class="name">' + escape(item.name) + '</span>' +
-                '</span>' +
-            '</div>';
-      }
-    }
-
-  })
+$('#friends').selectize({
+  create: false
 })
