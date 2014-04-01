568382eeee733491e7a484dbed1aca12bc1c97b1
Stanislas Marion <stanislas.marion@gmail.com>
2014-04-01 10:52:33 +0200

    images in search box!

--- client/main.js
+++ client/main.js
@@ -2,5 +2,11 @@ var jquery = require('jquery')
 require('selectize')
 
 $('#friends').selectize({
-  create: false
+  create: false,
+  placeholder: 'Sélectionner un(e) ami(e)...',
+  render: {
+    option: function (item, escape) {
+      return '<div><img src="' + escape(item.pic) + '"><span style="margin-left: 5px;">' + escape(item.text) + '</div>'
+    }
+  }
 })
