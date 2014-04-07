2de993ce9554411dbd5e99178c49225ba60d1654
Stanislas Marion <stanislas.marion@gmail.com>
2014-04-07 15:38:05 +0200

    rib checksum is now a validationerror. plus other validation errors

--- routes/rib.js
+++ routes/rib.js
@@ -6,14 +6,22 @@ module.exports = function (app) {
     var Rib = new model.Rib()
     Rib.bank = req.body.bank
     Rib.counter = req.body.counter
-    Rib.accountNumber = req.body.accountNumber
+    Rib.accountNumber = req.body.accountNumber.toUpperCase()
     Rib.key = req.body.key
     Rib.canonical = Rib.bank + Rib.counter + Rib.accountNumber + Rib.key
     Rib._user = req.user._id
     Rib.save(function (err, rib) {
-      console.log('biatch');
-      if (err) return next(err)
-      res.json({ title: 'Home', rib: rib.canonical })
+      if (err && err.name === 'ValidationError') {
+        _(err.errors).map(function (error) {
+          req.flash('error', error.message)
+        })
+        res.redirect('/')
+      } else if (err) {
+        next(err)
+      } else {
+        req.flash('success', 'RIB ajouté avec succès')
+        res.redirect('/')
+      }
     })
   })
 }
