c80971458d60293fc097ab057b9f1f88903bed08
Stanislas Marion <stanislas.marion@gmail.com>
2014-04-17 00:48:02 +0200

    lets try someting

--- model/index.js
+++ model/index.js
@@ -26,6 +26,7 @@ module.exports.connect = function (cb) {
   cb = once(cb)
   mongoose.set('debug', !config.isProd)
   mongoose.connect(config.mongo)
+  console.log('==================slap============================');
   mongoose.connection.on('error', cb)
   mongoose.connection.on('open', cb)
 }
