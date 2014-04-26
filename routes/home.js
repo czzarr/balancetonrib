abdab28de3b836542cfe68387a324dd54b57ccdb
Stanislas Marion <stanislas.marion@gmail.com>
2014-04-26 12:45:54 +0200

    woops

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 var ssl = require('../lib/ssl')
 
 module.exports = function (app) {
-  app.get('/', ssl.ensureSSL, unction (req, res, next) {
+  app.get('/', ssl.ensureSSL, function (req, res, next) {
     if (req.user)
       res.render('home')
     else
