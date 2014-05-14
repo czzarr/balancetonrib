68235bd72a1ed54636118da8f4c89ef63a0acc5f
Stanislas Marion <stanislas.marion@gmail.com>
2014-05-14 11:34:13 +0200

    remove bootflat from production build too

--- bin/cat-css.sh
+++ bin/cat-css.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cat bower_components/bootstrap/dist/css/bootstrap.min.css <(echo) \
   bower_components/font-awesome/css/font-awesome.min.css <(echo) \
-  bower_components/bootflat/css/bootflat.css <(echo) \
   bower_components/bootstrap-social/bootstrap-social.css <(echo) \
   bower_components/selectize/dist/css/selectize.bootstrap3.css <(echo) \
   out/css/main.css > out/css/balancetonrib.css
