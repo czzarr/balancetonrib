3c4724c1027a9486e131cae102547e45568b6b6f
Stanislas Marion <stanislas.marion@gmail.com>
2014-05-15 18:58:11 +0200

    country flags

--- bin/cat-css.sh
+++ bin/cat-css.sh
@@ -3,6 +3,7 @@ cat bower_components/bootstrap/dist/css/bootstrap.min.css <(echo) \
   bower_components/font-awesome/css/font-awesome.min.css <(echo) \
   bower_components/bootstrap-social/bootstrap-social.css <(echo) \
   bower_components/selectize/dist/css/selectize.bootstrap3.css <(echo) \
+  bower_components/flag-icon-css/css/flag-icon.css <(echo) \
   out/css/main.css > out/css/balancetonrib.css
 
 mv out/css/balancetonrib.css out/css/main.css
