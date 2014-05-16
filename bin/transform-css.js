48bbaafbbe4b24f8a889b379f63a94d14ad24d69
Stanislas Marion <stanislas.marion@gmail.com>
2014-05-16 10:57:40 +0200

    flag icons

--- bin/transform-css.js
+++ bin/transform-css.js
@@ -2,4 +2,5 @@ var fs = require('fs')
 
 var css = fs.readFileSync(process.argv[2], 'utf8')
 css = css.replace(/\.\.\/fonts/g, '/font-awesome/fonts')
+css = css.replace(/\.\.\/flags/g, '/flag-icon-css/flags')
 fs.writeFileSync(process.argv[2], css)
