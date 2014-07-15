9f3efbd27fc45d913a06f76ed9f69c952b682a02
Stanislas Marion <stanislas.marion@gmail.com>
2014-07-15 19:27:22 +0200

    next

--- index.js
+++ index.js
@@ -127,8 +127,8 @@ Site.prototype.addHeaders = function (req, res, next) {
  */
 Site.prototype.fetchFriends = function (req, res, next) {
   if (!req.user) return next()
-  console.dir('========ASDFASDFASDF=========', req);
-  console.dir('========BSDFBSDFBSDF=========', res);
+  console.log('========ASDFASDFASDF=========', req);
+  console.log('========BSDFBSDFBSDF=========', res);
   friendsdb.get(req.user.facebook, function (err, value) {
     if (err) {
       if (err.notFound) {
@@ -141,7 +141,7 @@ Site.prototype.fetchFriends = function (req, res, next) {
         var _res = res
         request.get(query, function (err, res, body) {
           if (err) return next(err)
-          console.dir('=====MOTHERFUCKER=======', res);
+          console.log('=====MOTHERFUCKER=======', res);
           var body = JSON.parse(body)
           var friends = body.data
           function savePagedResults (paging) {
